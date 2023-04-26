ea829a9b14e2d8cab414bca599c53d4bdca92473
apoorva2000ojha <63278396+apoorva2000ojha@users.noreply.github.com>
2023-04-26 16:05:39 +0530

    updating index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Logo from './Logo';
+// import Logo from './Logo';
 import Header from './header';
 import Form from './Form';
 // import Login from './Login';

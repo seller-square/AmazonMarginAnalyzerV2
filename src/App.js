5244133aefbe72e014c66dfe04abea0b4cf7e7c7
apoorva2000ojha <63278396+apoorva2000ojha@users.noreply.github.com>
2023-04-26 16:42:02 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import './App.css';
-import { Link } from 'react-router-dom';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Link data-testid="learn-react-link" to="/learn">Learn React</Link>
-      </header>
-    </div>
+    <div data-testid="specific-div" className="App">
+          </div>
   );
 }
-
-export default App;

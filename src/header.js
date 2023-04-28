43d7e61e6a41acdfd3d1782575a1f765af3a4675
Apoorva022000 <128361967+Apoorva022000@users.noreply.github.com>
2023-04-28 18:25:50 +0530

    social media icons

--- src/header.js
+++ src/header.js
@@ -8,14 +8,14 @@ function Header() {
       </div>
       <nav className="nav">
         <ul className="social-media">
-          <li><a href="https://www.instagram.com/sellersquare_in"><i className="fab fa-instagram"></i></a></li>
-          <li><a href="https://www.linkedin.com/company/seller-square"><i className="fab fa-linkedin"></i></a></li>
-          <li><a href="https://www.facebook.com/sellersquare"><i className="fab fa-facebook"></i></a></li>
-          <li><a href="https://www.youtube.com/@sellersquare"><i className="fab fa-youtube"></i></a></li>
-          <li><a href="https://twitter.com/sellersquare_in"><i className="fab fa-twitter"></i></a></li>
-          <li><a href="support@s"><i className="fas fa-envelope"></i></a></li>
+          <li><a href="https://www.instagram.com/sellersquare_in" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a></li>
+          <li><a href="https://www.linkedin.com/company/seller-square" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a></li>
+          <li><a href="https://www.facebook.com/sellersquare" target="_blank" rel="noreferrer"><i className="fab fa-facebook"></i></a></li>
+          <li><a href="https://www.youtube.com/@sellersquare" target="_blank" rel="noreferrer"><i className="fab fa-youtube"></i></a></li>
+          <li><a href="https://twitter.com/sellersquare_in" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a></li>
+          <li><a href="mailto:support@sellersquare.in" target="_blank" rel="noreferrer"><i className="fas fa-envelope"></i></a></li>
         </ul>
-        <button className="cta">Call Us</button>
+        <button className="cta" target="_blank" onclick="window.location.href='tel:8073881393'" rel="noreferrer">Call Us</button>
       </nav>
     </header>
   );

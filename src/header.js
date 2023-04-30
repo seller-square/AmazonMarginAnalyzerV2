b0ee4199063b3ce30919236b083e5dbdf44d11c6
Apoorva022000 <128361967+Apoorva022000@users.noreply.github.com>
2023-04-30 12:17:39 +0530

    adding dialer

--- src/header.js
+++ src/header.js
@@ -15,7 +15,7 @@ function Header() {
           <li><a href="https://twitter.com/sellersquare_in" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a></li>
           <li><a href="mailto:support@sellersquare.in" target="_blank" rel="noreferrer"><i className="fas fa-envelope"></i></a></li>
         </ul>
-        <button className="cta" target="_blank" onclick="window.location.href='tel:8073881393'" rel="noreferrer">Call Us</button>
+        <button className="cta" onClick={() => window.location.href='tel:8073881393'}>Call Us</button>
       </nav>
     </header>
   );
